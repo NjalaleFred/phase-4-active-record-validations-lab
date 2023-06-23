fd079a77df919ffa3f2a04c5a45250c97c1194d1
Njalale <fnjalale@gmail.com>
2023-06-23 18:00:51 +0300

    Completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+
+    validate :title_not_clickbaity
+
+    def title_not_clickbaity
+        clickbait_phrases = ["Won't Believe", "Secret", "Top", "Guess"]
+        unless title.present? && clickbait_phrases.any? { |phrase| title.include?(phrase) }
+            errors.add(:title, "must contain either 'Won't Believe', 'Secret', 'Top [number]', or 'Guess'")
+        end
+    end
+      
 end
